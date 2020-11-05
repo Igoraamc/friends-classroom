cc8497bb21d9df8972eaedfca9f48cf4d0893328
Igor Albanezi Conceição <iac@cesar.org.br>
2020-11-05 00:10:39 -0300

    first commit

---
+++ condition.js
@@ -0,0 +1,21 @@
+function plus(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function sub(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multi(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function div(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+var firstPlus = plus(7, 3);
+var secondPlus = sub(100, 300);
+
+console.log(firstPlus);
+console.log(secondPlus);
